832cc0b98ed051eca3febc516e648c92645f4471
lutfor2000 <lutfar199296@gmail.com>
2025-09-08 20:13:12 +0600

    Dropdown menu

--- Basic JavaScript/main.js
+++ Basic JavaScript/main.js
@@ -1,12 +1,28 @@
-// console.log(document)
-// console.log(document.head)
+const btn = document.getElementById("btn");
+const menuList = document.querySelector(".Product_list")
 
+// btn.addEventListener("click",() => {
+//     menuList.classList.toggle('.list_block');
+//     menuList.classList.remove('list_block')
+// }
+
+btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    menuList.classList.toggle("list_block");
+});
 // const url = document.URL;
 // console.log(url)
 
+// let leng = "Bangla";
 
+// function learn(topic){
+//     topic=leng;
+//     console.log(topic);
+// }
 
-//==============Part Two============
+// learn("English")
+
+// //==============Part Two============
 
 // const todo_id = document.getElementById('todu_id');
 
@@ -16,32 +32,32 @@
 // console.log(texC)
 // console.log(innText)
 
-//=========Select Class===========
+// //=========Select Class===========
 // const productList = document.getElementsByClassName('Product_list')
 // console.log(productList)
 
-//=======QuerySelector======
-const inputPart = document.querySelector('#input_part');
+// //=======QuerySelector======
+// const inputPart = document.querySelector('#input_part');
 
-inputPart.addEventListener('mouseover',()=>{
-    inputPart.style.background ="red";
-    inputPart.style.padding ="10px";
-})
+// inputPart.addEventListener('mouseover',()=>{
+//     inputPart.style.background ="red";
+//     inputPart.style.padding ="10px";
+// })
 
-inputPart.addEventListener('mouseout', () => {
-    inputPart.style.background = "";
-    inputPart.style.padding = "";
-});
+// inputPart.addEventListener('mouseout', () => {
+//     inputPart.style.background = "";
+//     inputPart.style.padding = "";
+// });
 
-//------------------querySelectorAll--------
+// //------------------querySelectorAll--------
 
-const inputFill = document.querySelectorAll('.in_item');
+// const inputFill = document.querySelectorAll('.in_item');
 
-inputFill.forEach(item =>{
-   item.addEventListener('click',()=>{
-    item.style.background = "green";
-    item.style.color = "red";
-   })
-})
+// inputFill.forEach(item =>{
+//    item.addEventListener('click',()=>{
+//     item.style.background = "green";
+//     item.style.color = "red";
+//    })
+// })
 
 // console.log(inputPart)
